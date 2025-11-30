ab1e01445c027ac9f00165f1e9f28a14a8ac0c4a
anJmya <a.figurin1@gmail.com>
2025-11-30 22:52:59 +0500

    KS-1-3 Gagarin flight info

---
+++ app/src/main/kotlin/lesson1/task3.kt
@@ -0,0 +1,14 @@
+package org.example.app.lesson1
+
+fun main() {
+    val year = 1961
+    var hour = "09"
+    var minutes = "07"
+    println(year)
+    println(hour)
+    println(minutes)
+
+    hour = "10"
+    minutes = "55"
+    print("$hour:$minutes\n")
+}
