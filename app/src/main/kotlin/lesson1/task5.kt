d889c0bf4c713faebe4cfd82f6ce15f320705eb9
anJmya <a.figurin1@gmail.com>
2025-12-01 19:15:31 +0500

    KS-1-5 Add constant for minutes and seconds. Remove magic number from equation

--- app/src/main/kotlin/lesson1/task5.kt
+++ app/src/main/kotlin/lesson1/task5.kt
@@ -1,12 +1,15 @@
 package org.example.app.lesson1
 
+const val SECONDS_PER_MINUTE = 60
+const val MINUTES_PER_HOUR = 60
+
 fun main() {
     val totalSeconds = 6480
 
-    var minutes = totalSeconds/60
-    val seconds = totalSeconds % 60
-    val hours = minutes/60
-    minutes %= 60
+    var minutes = totalSeconds/SECONDS_PER_MINUTE
+    val seconds = totalSeconds % SECONDS_PER_MINUTE
+    val hours = minutes/MINUTES_PER_HOUR
+    minutes %= MINUTES_PER_HOUR
 
-    println ("%02d:%02d:%02d".format(hours,minutes,seconds))
+    println("%02d:%02d:%02d".format(hours,minutes,seconds))
 }
