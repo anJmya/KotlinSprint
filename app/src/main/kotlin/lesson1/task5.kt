c7efaa583fb72a5baa347602e9949b7c99e3d935
anJmya <a.figurin1@gmail.com>
2025-12-03 00:35:31 +0500

    KS-2-5 reformating

--- app/src/main/kotlin/lesson1/task5.kt
+++ app/src/main/kotlin/lesson1/task5.kt
@@ -6,9 +6,9 @@ const val MINUTES_PER_HOUR = 60
 fun main() {
     val totalSeconds = 6480
 
-    var minutes = totalSeconds/SECONDS_PER_MINUTE
+    var minutes = totalSeconds / SECONDS_PER_MINUTE
     val seconds = totalSeconds % SECONDS_PER_MINUTE
-    val hours = minutes/MINUTES_PER_HOUR
+    val hours = minutes / MINUTES_PER_HOUR
     minutes %= MINUTES_PER_HOUR
 
     println("%02d:%02d:%02d".format(hours,minutes,seconds))
