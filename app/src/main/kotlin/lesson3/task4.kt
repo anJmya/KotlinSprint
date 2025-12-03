ac97df59cfdae56ecac95fb683816d5b9a7d157a
anJmya <a.figurin1@gmail.com>
2025-12-03 21:32:01 +0500

    KS-3-4 Chess data

---
+++ app/src/main/kotlin/lesson3/task4.kt
@@ -0,0 +1,14 @@
+package org.example.app.lesson3
+
+fun main() {
+    var startingPoint = "E2"
+    var endPoint = "E4"
+    var step = 1
+
+    println("[$startingPoint-$endPoint;$step]")
+
+    startingPoint = "D2"
+    endPoint = "D3"
+    step = 2
+    println("[$startingPoint-$endPoint;$step]")
+}
