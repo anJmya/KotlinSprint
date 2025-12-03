b56dd35b3ad90ed21e4e3509fddf62eb12a47fd6
anJmya <a.figurin1@gmail.com>
2025-12-03 23:41:44 +0500

    KS-3-3 multiply table

--- app/src/main/kotlin/lesson3/task3.kt
+++ app/src/main/kotlin/lesson3/task3.kt
@@ -3,15 +3,15 @@ package org.example.app.lesson3
 fun main() {
     val number = 4
 
-    println(
-        "$number * 1 = ${number * 1}\n" +
-        "$number * 2 = ${number * 2}\n" +
-        "$number * 3 = ${number * 3}\n" +
-        "$number * 4 = ${number * 4}\n" +
-        "$number * 5 = ${number * 5}\n" +
-        "$number * 6 = ${number * 6}\n" +
-        "$number * 7 = ${number * 7}\n" +
-        "$number * 8 = ${number * 8}\n" +
-        "$number * 9 = ${number * 9}\n"
-    )
+    println("""
+        $number * 1 = ${number * 1}
+        $number * 2 = ${number * 2}
+        $number * 3 = ${number * 3}
+        $number * 4 = ${number * 4}
+        $number * 5 = ${number * 5}
+        $number * 6 = ${number * 6}
+        $number * 7 = ${number * 7}
+        $number * 8 = ${number * 8}
+        $number * 9 = ${number * 9}    
+    """.trimIndent())
 }
