bcf315e54dfb3a9b16f34d063e46e8bca7c75a20
anJmya <a.figurin1@gmail.com>
2025-12-04 00:17:23 +0500

    KS-3-5 chess data parsing. Split func optimization

--- app/src/main/kotlin/lesson3/task5.kt
+++ app/src/main/kotlin/lesson3/task5.kt
@@ -3,13 +3,10 @@ package org.example.app.lesson3
 fun main() {
     val moveData = "D2-D4;0"
 
-    val splitData = moveData.split(";")
-    val position = splitData[0]
-    val step = splitData[1]
-
-    val splitPosition = position.split("-")
-    val staringPoint = splitPosition[0]
-    val endPoint = splitPosition[1]
+    val splitData = moveData.split("-" , ";")
+    val staringPoint = splitData[0]
+    val endPoint = splitData[1]
+    val step = splitData[2]
 
     println(staringPoint)
     println(endPoint)
