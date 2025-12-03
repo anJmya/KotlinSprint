c2695e6e29c28d18ccee8155dc6ce6e079f70538
anJmya <a.figurin1@gmail.com>
2025-12-03 20:52:49 +0500

    KS-3-1 Individual greeting

---
+++ app/src/main/kotlin/lesson3/task1.kt
@@ -0,0 +1,12 @@
+package org.example.app.lesson3
+
+fun main() {
+    val userName = "Антон"
+    var greeting = "Добрый день"
+
+    println("$greeting, $userName")
+
+    greeting = "Добрый вечер"
+
+    println("$greeting, $userName")
+}
