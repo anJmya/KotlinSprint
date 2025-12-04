60c996f67d1abcacb1a3a797b6e950e518c37e59
anJmya <a.figurin1@gmail.com>
2025-12-04 21:09:16 +0500

    KS-4-3 formating with detekt, move condition value to constant

--- app/src/main/kotlin/lesson4/task3.kt
+++ app/src/main/kotlin/lesson4/task3.kt
@@ -1,5 +1,10 @@
 package org.example.app.lesson4
 
+const val SUNNY: Boolean = true
+const val TENT: Boolean = true
+const val REQUIRED_HUMIDITY: Int = 20
+const val FORBIDDEN_SEASON: String = "Зима"
+
 fun main() {
     // Условия сейчас
     val isSunny: Boolean = true
@@ -8,8 +13,10 @@ fun main() {
     val season: String = "Зима"
 
     // Благоприятные условия
-    val isGoodCondition = isSunny && tentOpen && humidity == 20 && season != "Зима"
+    val isGoodCondition = isSunny == SUNNY &&
+        tentOpen == TENT &&
+        humidity == REQUIRED_HUMIDITY &&
+        season != FORBIDDEN_SEASON
 
     println("Благоприятные ли условия сейчас для роста бобовых? $isGoodCondition")
-
 }
