7350ba617b0f7ba78c5a5c12f08672535db98fda
anJmya <a.figurin1@gmail.com>
2025-12-04 19:30:58 +0500

    KS-4-1 Total table moved to const val; rewrite availability logic

--- app/src/main/kotlin/lesson4/task1.kt
+++ app/src/main/kotlin/lesson4/task1.kt
@@ -1,12 +1,13 @@
 package org.example.app.lesson4
 
+const val TOTAL_TABLE = 13
+
 fun main() {
-    val totalTable = 13
     val bookedTableToday = 13
     val bookedTableTomorrow = 9
 
-    val isAllowedToday = totalTable != bookedTableToday
-    val isAllowedTomorrow = totalTable != bookedTableTomorrow
+    val isAllowedToday = TOTAL_TABLE > bookedTableToday
+    val isAllowedTomorrow = TOTAL_TABLE > bookedTableTomorrow
 
     println("Доступность столиков на сегодня: $isAllowedToday \n" +
             "Доступность столиков на завтра: $isAllowedTomorrow")
