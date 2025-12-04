526153a9a0715fe14176a699ba66b39176f779e9
anJmya <a.figurin1@gmail.com>
2025-12-04 13:32:06 +0500

    KS-3-5 chess data parsing. reformating

--- app/src/main/kotlin/lesson3/task5.kt
+++ app/src/main/kotlin/lesson3/task5.kt
@@ -3,12 +3,12 @@ package org.example.app.lesson3
 fun main() {
     val moveData = "D2-D4;0"
 
-    val splitData = moveData.split("-" , ";")
-    val staringPoint = splitData[0]
+    val splitData = moveData.split("-", ";")
+    val startingPoint = splitData[0]
     val endPoint = splitData[1]
     val step = splitData[2]
 
-    println(staringPoint)
+    println(startingPoint)
     println(endPoint)
     println(step)
 }
