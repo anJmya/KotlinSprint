c437ab52dc5baacd9a0fdea082000c50a4695785
anJmya <a.figurin1@gmail.com>
2025-12-04 20:47:33 +0500

    KS-4-2 formating with detekt, move condition value to constant

--- app/src/main/kotlin/lesson4/task2.kt
+++ app/src/main/kotlin/lesson4/task2.kt
@@ -1,16 +1,21 @@
 package org.example.app.lesson4
 
-fun main() {
-    val averageWeightMin = 35
-    val averageWeightMax = 100
-    val averageVolume = 100
+const val WEIGHT_MIN = 35
+const val WEIGHT_MAX = 100
+const val VOLUME = 100
 
+fun main() {
     val weight1 = 20
     val volume1 = 80
-    println("Груз с весом $weight1 кг и объемомм $volume1 л соотвествует категории 'Average': ${weight1 > averageWeightMin && weight1 <= averageWeightMax && volume1 > averageVolume}")
+    println(
+        "Груз с весом $weight1 кг и объемомм $volume1 л соотвествует категории 'Average':" +
+            " ${weight1 > WEIGHT_MIN && weight1 <= WEIGHT_MAX && volume1 > VOLUME}",
+    )
 
     val weight2 = 50
     val volume2 = 100
-    println("Груз с весом $weight2 кг и объемомм $volume2 л соотвествует категории 'Average': ${weight2 > averageWeightMin && weight2 <= averageWeightMax && volume2 > averageVolume}")
-
+    println(
+        "Груз с весом $weight2 кг и объемомм $volume2 л соотвествует категории 'Average':" +
+            " ${weight1 > WEIGHT_MIN && weight1 <= WEIGHT_MAX && volume1 > VOLUME}",
+    )
 }
